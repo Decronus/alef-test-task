2afe72cc3f33127ac525a8d089ef433dc03c2554
Decronus <zick3333@mail.ru>
2023-05-25 01:41:17 +0200

    parent card

---
+++ src/components/ParentCardComp.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="parent-card-wrap">
+        <div class="parent-card-personal-data">
+            <h3>Персональные данные</h3>
+            <preview-h3-comp>{{ parent.name }}, {{ parent.age }} {{ getYearsEnding(parent.age) }}</preview-h3-comp>
+        </div>
+
+        <div class="parent-card-children-data">
+            <h3>Дети</h3>
+            <child-card-comp v-for="child in parent.children" :key="child.id"
+                >{{ child.name }}, {{ child.age }} {{ getYearsEnding(child.age) }}</child-card-comp
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+import ChildCardComp from "@/components/ChildCardComp.vue";
+import PreviewH3Comp from "../components/PreviewH3Comp.vue";
+
+export default {
+    name: "parent-card-comp",
+    components: { PreviewH3Comp, ChildCardComp },
+
+    props: {
+        parent: {
+            required: true,
+            type: Object,
+        },
+    },
+
+    methods: {
+        getYearsEnding(age) {
+            if (age >= 11 && age <= 14) {
+                return "лет";
+            } else {
+                const lastDigit = age % 10;
+                if (lastDigit === 1) {
+                    return "год";
+                } else if (lastDigit >= 2 && lastDigit <= 4) {
+                    return "года";
+                } else {
+                    return "лет";
+                }
+            }
+        },
+    },
+};
+</script>
+
+<style>
+.parent-card-wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.parent-card-personal-data {
+    margin-bottom: 60px;
+}
+</style>
--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -1,45 +1,16 @@
 <template>
     <div class="preview-page-wrap">
-        <div class="preview-personal-and-children" v-for="parent in $store.state.parents" :key="parent.id">
-            <div class="preview-personal-data">
-                <h3>Персональные данные</h3>
-                <preview-h3-comp>{{ parent.name }}, {{ parent.age }} {{ getYearsEnding(parent.age) }}</preview-h3-comp>
-            </div>
-
-            <div class="preview-children-data">
-                <h3>Дети</h3>
-                <child-card-comp v-for="child in parent.children" :key="child.id"
-                    >{{ child.name }}, {{ child.age }} {{ getYearsEnding(child.age) }}</child-card-comp
-                >
-            </div>
-        </div>
+        <h3 v-if="$store.state.parents.length === 0">Нет данных для отображения</h3>
+        <parent-card-comp v-else v-for="parent in $store.state.parents" :parent="parent" :key="parent.id" />
     </div>
 </template>
 
 <script>
-import ChildCardComp from "@/components/ChildCardComp.vue";
-import PreviewH3Comp from "../components/PreviewH3Comp.vue";
+import ParentCardComp from "@/components/ParentCardComp.vue";
 
 export default {
     name: "preview-page",
-    components: { PreviewH3Comp, ChildCardComp },
-
-    methods: {
-        getYearsEnding(age) {
-            if (age >= 11 && age <= 14) {
-                return "лет";
-            } else {
-                const lastDigit = age % 10;
-                if (lastDigit === 1) {
-                    return "год";
-                } else if (lastDigit >= 2 && lastDigit <= 4) {
-                    return "года";
-                } else {
-                    return "лет";
-                }
-            }
-        },
-    },
+    components: { ParentCardComp },
 };
 </script>
 
@@ -52,16 +23,4 @@ export default {
 .preview-page-wrap > div:not(:last-child) {
     margin-bottom: 100px;
 }
-
-.preview-personal-and-children {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-}
-
-.preview-personal-data {
-    margin-bottom: 60px;
-}
 </style>
-
-PreviewH3Comp
